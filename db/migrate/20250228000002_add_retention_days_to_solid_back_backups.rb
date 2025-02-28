f68f2f767b5f82468579cc32c19d465e5c58e488
florian2 <fgoersdorf@gmail.com>
2025-02-28 16:45:56 +0100

    renamed migration

--- db/migrate/20250228000002_add_retention_days_to_solid_back_backups.rb
+++ db/migrate/20250228000002_add_retention_days_to_solid_back_backups.rb
@@ -1,4 +1,4 @@
-class AddRetentionDaysToBackups < ActiveRecord::Migration[7.0]
+class AddRetentionDaysToSolidBackupBackups < ActiveRecord::Migration[7.0]
   def change
     add_column :solid_backup_backups, :retention_days, :integer, default: 7
   end
